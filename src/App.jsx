0c565d46a0f4a28d72be5ee719de35c021641530
Kashif Khan <KashifKhnx04@gmail.com>
2023-09-23 08:23:12 +0500

    adding Die components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import Die from "./components/Die"
+
 
 function App() {
 
+  function allNewDice () {
+    const newDice = [];
+    for(let i=0; i<10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice;
+  }
+
+  console.log(allNewDice())
   return (
     <div className="App">
-      <main className="main">
-        
+      <main>
+        <div className="dice-container">
+          <Die value='1' />
+          <Die value='2' />
+          <Die value='3' />
+          <Die value='4' />
+          <Die value='5' />
+          <Die value='6' />
+          <Die value='0' />
+          <Die value='1' />
+          <Die value='3' />
+          <Die value='4' />
+        </div>
       </main>
     </div>
   )
---
+++ src/components/Die.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Die = (props) => {
+  return (
+    <div className='die-face'>
+        <h2 className='die-num'>{props.value}</h2>
+    </div>
+  )
+}
+
+export default Die
