7ba93e410bdfea1c00c9ac4d0d1556ace903a4ed
Kashif Khan <KashifKhnx04@gmail.com>
2023-09-23 09:03:34 +0500

    change dice from array to object also adding unique id to every element

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import Die from "./components/Die"
+import { nanoid } from "nanoid";
 
 
 function App() {
   const [dice, setDice] = useState(allNewDice);
-  
-  function allNewDice () {
+
+  function allNewDice() {
     const newDice = [];
-    for(let i=0; i<10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+    for (let i = 0; i < 10; i++) {
+      newDice.push(
+        {
+          id: nanoid(),
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false
+        }
+      )
     }
     return newDice;
   }
 
-  function handleRollDices () {
+  function handleRollDices() {
     setDice(allNewDice)
   }
 
-  const allDiceElements = dice.map(die => <Die value={die}/>)
+  const allDiceElements = dice.map(die => <Die key={die.id} value={die.value} />)
 
-  console.log(allNewDice())
   return (
     <div className="App">
       <main>
